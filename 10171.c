98ecc55edd1c3dd8fb9ae71c4ac1980fb9dde941
Jeongheoni <jeongheoni494@gmail.com>
2026-03-29 19:18:56 +0900

    10171번 문제 추가

---
+++ 10171.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|");
+    return 0;
+}
+
+
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ */

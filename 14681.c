19085b040af833eb40d50aaa03722a4373ddc74d
Jeongheoni <jeongheoni494@gmail.com>
2026-03-31 20:08:02 +0900

    9498번, 14681번, 2741번 문제 추가

---
+++ 14681.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y;
+    
+    scanf("%d %d", &x, &y);
+    
+    if (x < 0) {
+        if (y < 0) {
+            printf("3");
+        } else {
+            printf("2");
+        }
+    } else {
+        if (y < 0) {
+            printf("4");
+        } else {
+            printf("1");
+        }
+    }
+    
+    return 0;
+}

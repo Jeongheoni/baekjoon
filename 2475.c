f89a792c7e98868194eda3c0ecdfa40a4ff1312e
Jeongheoni <jeongheoni494@gmail.com>
2026-04-07 21:04:06 +0900

    2475번 문제 추가

---
+++ 2475.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b = 0;
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &a);
+        b += a * a;
+    }
+    printf("%d\n", b % 10);
+    return 0;
+}
